f0f28c3fe83eba68fbe7a63e7050c8798a5d7b2a
nmhillusion <nguyenminhhieu.geek@gmail.com>
2024-10-27 11:48:33 +0700

    impl sitemap for pages

--- build.mjs
+++ build.mjs
@@ -1,6 +1,8 @@
 import { BullEngine } from "@nmhillusion/n2ngin-bull-engine";
 import { parser } from "@nmhillusion/n2mix";
 import * as path from "path";
+import * as fs from "fs";
+import { Indexity } from "@nmhillusion/n2ngin-indexity";
 
 const dirname = process.cwd();
 
@@ -18,6 +20,19 @@ new BullEngine()
   .config({
     copyResource: {
       enabled: true,
+      config: {
+        extsToCopy: [
+          ".jpg",
+          ".jpeg",
+          ".png",
+          ".gif",
+          ".ico",
+          ".woff",
+          ".ttf",
+          ".yml",
+          ".yaml",
+        ],
+      },
     },
     markdown: {
       enabled: true,
@@ -45,3 +60,15 @@ new BullEngine()
   })
   .setVariableFilePathToInject(path.join(dirname, envPath))
   .render();
+
+const indexity_ = new Indexity().config({
+  relativeTo: "dist",
+  srcDir: path.join(dirname, "dist"),
+  baseHref: ".",
+});
+
+const indexityBuilder_ = await indexity_.build();
+
+const siteMapContent = indexityBuilder_.operator.html.basicUI();
+
+fs.writeFileSync("dist/index.html", siteMapContent);
